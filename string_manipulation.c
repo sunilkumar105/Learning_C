55e3b09ca0ec4569ed6c452731b2451c818bcf3d
sunil <sunilkumar62394@gmail.com>
2025-09-02 20:20:58 +0530

    string manipulation added

--- string_manipulation.c
+++ string_manipulation.c
@@ -42,5 +42,49 @@ int main()
         } while (ptr != NULL);
     }
 
+    /*Search character in a string (1st occurance)*/
+    char name[10] = "Suninl";
+    char *posOf_n = strchr(name, 'n');
+    if (posOf_n)
+    {
+        printf("n is available at memory address: %p, and at location: %d \r\n", posOf_n, posOf_n - name);
+    }
+    else
+    {
+        printf("Couldnt find character n in the string: %s \r\n", name);
+    }
+
+    /*Search character in string (last occurance)*/
+    char *posOfLast_n = strrchr(name, 'n');
+    if (posOfLast_n)
+    {
+        printf("n is available at memory address of last address: %p, and at location: %d \r\n", posOfLast_n, posOfLast_n - name);
+    }
+    else
+    {
+        printf("Couldnt find character n in the string: %s \r\n", name);
+    }
+
+    /*Concatenate string*/
+    char mainStr[100] = "Sunil";
+    char *destStrPointer = strcat(mainStr, " Kumar");
+    printf("Concatenated strin is %s \r\n", mainStr);
+    if (destStrPointer == mainStr)
+    {
+        printf("String concatenated \r\n");
+    }
+    else
+    {
+        printf("Concatenation failed \r\n");
+    }
+
+    /*Compare two string*/
+    char str1[100] = "sunil";
+    char *str1_addr = strcmp(str1, "sunil");
+    if (str1_addr == NULL)
+    {
+        printf("Both the string are equal\r\n");
+    }
+
     return 0;
 }
