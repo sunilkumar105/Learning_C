cb21248e750ff3fe63492dbb69a1e10e156530a8
sunil kumar <sunil@wbe.in>
2025-09-30 17:43:47 +0530

    working on string maniupulation 1

--- string_manipulation1.c
+++ string_manipulation1.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <ctype.h>
 
 int main()
 {
 
-
     /*Find whether a byte is a number, character or what*/
     uint8_t byteArr[20] = {'1', 'a', '3', 'F', 0x12, 0x01};
-
-    is_alp
+    for (int i = 0; i < 5; i++)
+    {
+        if (isalpha(byteArr[i]))
+        {
+            printf("Index: %c is a alphanumeric \r\n", byteArr[i]);
+        }
+    }
 
     return 0;
 }
